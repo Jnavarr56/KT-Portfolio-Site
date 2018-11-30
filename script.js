aba75d1db0df34f7c29449c8c8a306625d0b5be5
Jorge Navarro <jorgenavarro@Jorges-MacBook-Pro.local>
2018-11-29 22:43:30 -0500

    initial landing set up, oop approach set up

--- script.js
+++ script.js
@@ -1,4 +1,55 @@
-$( document ).ready( function() {
+class Kat { 
+    constructor() {
+        this.animPhrases = [];
+        this.phraseTextID = null;
+        this.phraseTextIDMobile = null;
+    }
+    setPhraseAnimationElements( phrasesArray, phraseElementID, phraseElementIDMobile = null ) {
+        this.animPhrases = phrasesArray;
+        this.phraseTextID = phraseElementID;
+        this.phraseTextIDMobile = phraseElementIDMobile;
+    }
+    phraseAnimate() {
+        
+        let self = this,
+            phraseElement = $( '#' + this.phraseTextID ),
+            phraseIndex = 1;
 
+        phraseElement.html( self.animPhrases[ 0 ].toUpperCase());
+
+        
+        setInterval( function() {
+            phraseElement.html( self.animPhrases[ phraseIndex ].toUpperCase() );
+            phraseIndex === self.animPhrases.length - 1 ? phraseIndex = 0 : phraseIndex ++;
+        }, 2000 );
+        
+
+
+
+
+
+    }
+    phraseAnimateMobile() {
+        //console.log( 'Test animate 2' );
+    }
+}
+var phrases = [
+    'Junior Developer',
+    'Polish Speaker',
+    'Coffee Addict',
+    'Former English Teacher',
+    'Amateur Sociologist',
+    'Bouldering Fanatic',
+    'HS Theatre Geek',
+];
+
+$( document ).ready( function() {
+    
+    var Katrina = new Kat();
+    Katrina.setPhraseAnimationElements( phrases, 'bounce-text', 'bounce-text-mobile' );
+    Katrina.phraseAnimate();
+    Katrina.phraseAnimateMobile();
+    
+    
     
 } );
